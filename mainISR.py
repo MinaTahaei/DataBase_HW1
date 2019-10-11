801e5da1e0f5c0108ba956db1c88c314ebeffa48
Mina Tahaei <bluxixi@outlook.com>
2019-10-11 20:38:18 +0330

    basic find book, basic remove book

--- mainISR.py
+++ mainISR.py
@@ -3,7 +3,6 @@ def addBook (userInput):
     Tmp[0] = Tmp[0][10:]
     with open('books.txt', 'a+') as RW:
       numLines = sum(1 for line in open('books.txt')) 
-    #   numLines = RW.readlines()
       count = numLines - 1
       RW.write(str(count) + '-')
       for item in Tmp:
@@ -15,8 +14,26 @@ def findBook (userInput):
     Tmp[0] = Tmp[0][10:]
     # print(Tmp[0]):the real search part
     # print(Tmp[1]):the general search part
+    with open ('books.txt', 'a+') as RF:
+        line = RF.readlines()   
+        for lines in line:
+            Tmp2 = line.split('/')
+            Tmp2[0] = Tmp2[0][2:]
+            for i in range(0, 6):   
+                bmp[i] = Tmp2[i].split(':')
+                
+             
 
-# def removeBook (userInput):
+ def removeBook (userInput):
+     Tmp3 = userInput.split(' ')
+     Tmp3[0] = Tmp3[0][12:]
+     with open ('books.txt', 'a+') as RM:
+         line = RM.readlines()
+         for lines in line:         
+             if(Tmp3[0] ==  )
+
+        
+
 # def updateBook (userInput):
 # def addPublisher(userInput):
 # def findPublisher(userInput):
