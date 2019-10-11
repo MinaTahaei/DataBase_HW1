ec2cd2c2aebb6bd63415599cf60a716d35166af2
Mina Tahaei <bluxixi@outlook.com>
2019-10-11 19:31:55 +0330

    complete add book method

--- mainISR.py
+++ mainISR.py
@@ -2,18 +2,13 @@ def addBook (userInput):
     Tmp = userInput.split(' , ')
     Tmp[0] = Tmp[0][10:]
     with open('books.txt', 'a+') as RW:
-        # for i in range (1, len(Tmp)):
-        #     for item in Tmp:
-        #      RW.write(str(i) + '-')
-        #      RW.write(item + '/ ') 
-        #      RW.write('\n')
-     numLines = RW.readlines()
-     count = len(numLines + 1)
-    #  numLines = sum(1 for line in open('books.txt'))
-     RW.write(str(count) + '-')
-    for item in Tmp:
-        RW.write(item + '/ ')
-    RW.write('\n')
+      numLines = sum(1 for line in open('books.txt')) 
+    #   numLines = RW.readlines()
+      count = numLines - 1
+      RW.write(str(count) + '-')
+      for item in Tmp:
+         RW.write(item + '/ ')
+      RW.write('\n')
 
 def findBook (userInput):
     Tmp = userInput.split(' by ')
@@ -21,12 +16,12 @@ def findBook (userInput):
     # print(Tmp[0]):the real search part
     # print(Tmp[1]):the general search part
 
-def removeBook (userInput):
-def updateBook (userInput):
-def addPublisher(userInput):
-def findPublisher(userInput):
-def removePublisher (userInput):
-def updatePublisher (userInput):
+# def removeBook (userInput):
+# def updateBook (userInput):
+# def addPublisher(userInput):
+# def findPublisher(userInput):
+# def removePublisher (userInput):
+# def updatePublisher (userInput):
 
 
 print('for adding books please press A and for Finding boks please press F')
